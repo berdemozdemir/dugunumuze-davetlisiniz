0d886ca3b44d4ca61374576552fcafe308e6bad3
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2026-04-07 17:39:53 +0300

    feat: implement authentication session retrieval and client session management

---
+++ modules/auth/actions/get-client-session.ts
@@ -0,0 +1,33 @@
+import { createClient } from '@/integrations/supabase/supabase-server';
+import { err, ok } from '@/lib/result';
+
+export const getClientSession = async () => {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase.auth.getUser();
+
+  if (!data.user)
+    return err({
+      reason: 'user-is-not-logged-in',
+      message: 'User is not logged in',
+    });
+
+  if (error !== null) {
+    return err({
+      message: error.message,
+      reason: 'supabase-error',
+    });
+  }
+
+  if (!data.user.email)
+    return err({
+      message: 'User does not have an email',
+      reason: 'email-missing',
+    });
+
+  return ok({
+    userId: data.user.id,
+    userEmail: data.user.email,
+    userMetadata: data.user.user_metadata,
+  });
+};
