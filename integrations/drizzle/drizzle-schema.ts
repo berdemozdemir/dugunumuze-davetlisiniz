0ed499ea41ed5a911d8599a45690297df934282a
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2026-04-07 18:01:44 +0300

    feat: integrate drizzle orm

---
+++ integrations/drizzle/drizzle-schema.ts
@@ -0,0 +1,20 @@
+/**
+ * What the heck is this file?
+ *
+ * This file imports all of our schemas into one place so we can give it to the drizzle
+ * client later for use with its query api.
+ *
+ * See @https://orm.drizzle.team/docs/sql-schema-declaration#organize-your-schema-files
+ */
+
+// ## MODELS ##
+import * as userSchema from "@/modules/auth/db-tables";
+
+// ## RELATIONS ##
+
+// (e.g. if you have relations)
+// import * as userRelations from "@/modules/auth/db-relations";
+
+export const drizzleSchema = {
+  ...userSchema,
+};
