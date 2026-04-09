7df62988b34792a56b10022799510ef09f8d1b9e
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2026-04-09 17:31:54 +0300

    feat: add slugify package and implement SlugHelper utility for generating slugs

---
+++ lib/utils/slug.ts
@@ -0,0 +1,21 @@
+import slugify from 'slugify';
+
+export const SlugHelper = {
+  generate: (str: string) =>
+    slugify(str, {
+      replacement: '-',
+      remove: undefined,
+      lower: true,
+      strict: true,
+      trim: true,
+    }),
+
+  generateUnique: (str: string, suffix: string) => {
+    if (suffix.length > 8) suffix = suffix.slice(0, 8);
+
+    suffix = suffix.replace(/-/g, '');
+
+    return `${SlugHelper.generate(str)}-${suffix}`;
+  },
+};
+
