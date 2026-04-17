cf19c538a075b7e72724b6649e57850985056888
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2026-04-18 01:04:18 +0300

    feat: implement middleware for session management and authentication redirection

--- proxy.ts
+++ proxy.ts
@@ -1,11 +1,11 @@
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { updateSession } from '@/integrations/supabase/middleware';
+import { paths } from '@/lib/paths';
+import { createClient } from './integrations/supabase/supabase-server';
 
-// TODO: add use auth middleware here, can cheat from udao
 // TODO: add use i18n middleware here, can cheat from udao
-// TODO: user should not go to landing page if they are logged in
 export async function proxy(request: NextRequest) {
-  return await updateSession(request);
+  return runMiddleware(request, [useSessionMiddleware, useAuthMiddleware]);
 }
 
 export const config = {
@@ -19,3 +19,79 @@ export const config = {
     '/((?!_next/static|_next/image|favicon.ico).*)',
   ],
 };
+
+type Middleware = (
+  request: NextRequest,
+  response: NextResponse,
+) => Promise<NextResponse>;
+
+/** Runs given middleware on the given request in order */
+async function runMiddleware(request: NextRequest, middlewares: Middleware[]) {
+  let response = NextResponse.next();
+
+  for (const middleware of middlewares) {
+    const newResponse = await middleware(request, response);
+
+    if (newResponse) {
+      response = newResponse;
+
+      if (newResponse.status === 307 || newResponse.status === 308) {
+        return newResponse;
+      }
+    }
+  }
+
+  return response;
+}
+
+async function useSessionMiddleware(
+  request: NextRequest,
+  _response: NextResponse,
+) {
+  return updateSession(request);
+}
+
+/**
+ * Authenticated users cannot view the marketing home page; send them to the dashboard.
+ */
+async function useAuthMiddleware(request: NextRequest, response: NextResponse) {
+  const protectedPages = [paths.dashboard.base] as const;
+  const authPages = ['auth'] as const;
+
+  const isAccessingProtectedPage =
+    protectedPages.filter((pageUrl) =>
+      request.nextUrl.pathname.startsWith(pageUrl),
+    ).length > 0;
+
+  const isAccessingLandingPage = request.nextUrl.pathname === '/';
+
+  const isAccessingAuthPage =
+    authPages.filter((pageUrl) => request.nextUrl.pathname.startsWith(pageUrl))
+      .length > 0;
+
+  const supabase = await createClient();
+  const auth = await supabase.auth.getUser();
+
+  const isLoggedIn = !auth.error && !!auth.data;
+
+  // redirect unauthenticated users attempting to access protected pages to login (with callback)
+  if (!isLoggedIn && isAccessingProtectedPage) {
+    const callbackUrl = request.nextUrl.pathname;
+    return NextResponse.redirect(
+      new URL(`${paths.auth.login}?callback_url=${callbackUrl}`, request.url),
+    );
+  }
+
+  // authenticated users should not go to the landing page
+  if (isLoggedIn && isAccessingLandingPage) {
+    return NextResponse.redirect(new URL(paths.dashboard.base, request.url));
+  }
+
+  // authenticated users should not go to the auth pages
+  if (isLoggedIn && isAccessingAuthPage) {
+    return NextResponse.redirect(new URL(paths.dashboard.base, request.url));
+  }
+
+  // otherwise, everything is a-ok.
+  return response;
+}
